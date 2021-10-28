67e7a380f410bc07ea481abd07cbbe101001636d
ABHIJITH WARRIER <46685919+abhijithwarrier@users.noreply.github.com>
2021-10-28 10:04:26 +0530

    Update PythonDictionary.py

--- PythonDictionary.py
+++ PythonDictionary.py
@@ -2,11 +2,9 @@
 
 # PYTHON GUI TO GET THE MEANING OF USER-ENTERED WORDS USING THE PyDictionary Module
 
-# PyDictionary is a Dictionary Module for Python 2/3 to get meanings, translations,
-# synonyms and Antonyms of words. It uses WordNet for getting meanings, Google for
-# translations, and synonym.com for getting synonyms and antonyms. This module uses
-# Python Requests, BeautifulSoup4 and goslate as dependencies
-#
+# PyDictionary is a Dictionary Module for Python 2/3 to get meanings, translations, synonyms and Antonyms of words. It uses WordNet for getting meanings, Google for
+# translations, and synonym.com for getting synonyms and antonyms. This module uses Python Requests, BeautifulSoup4 and goslate as dependencies
+
 # The module can be installed using the command - pip install PyDictionary
 
 # Importing necessary packages
